32306d2cbb0bf6b0d159785f0d56112481c9bb7c
lohith <viperlohith121@gmail.com>
2021-10-23 22:14:54 +0530

    added default theme

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider defaultTheme="dark" attribute="class">
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
