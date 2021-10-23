8f1fb1e4b914fe7baf21d5a98a15ae1f66b648dd
lohith <viperlohith121@gmail.com>
2021-10-23 22:07:45 +0530

    added article js code performance

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/projects', title: 'Projects' },
 ]
 
 export default headerNavLinks
