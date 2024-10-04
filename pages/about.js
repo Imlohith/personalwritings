c4d4ef4e3d52b891363afb96a652137f321bfc8b
lohith <viperlohith121@gmail.com>
2024-10-04 14:31:48 +0530

    resolve

--- pages/about.js
+++ pages/about.js
@@ -4,11 +4,26 @@ import { getFileBySlug } from '@/lib/mdx'
 const DEFAULT_LAYOUT = 'AuthorLayout'
 
 export async function getStaticProps() {
-  const authorDetails = await getFileBySlug('authors', ['default'])
-  return { props: { authorDetails } }
+  try {
+    const authorDetails = await getFileBySlug('authors', ['default'])
+    return {
+      props: { authorDetails },
+      source: 'pages/about.js',
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      props: {},
+      source: 'pages/about.js',
+    }
+  }
 }
 
 export default function About({ authorDetails }) {
+  if (!authorDetails || !authorDetails.mdxSource || !authorDetails.frontMatter) {
+    return <div>Error: Invalid author details</div>
+  }
+
   const { mdxSource, frontMatter } = authorDetails
 
   return (
